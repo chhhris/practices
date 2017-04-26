2e09b3040d49e820d3f3e3fb981733ec0120418c
Christopher Lake <mr.lake@gmail.com>
2017-04-26 10:09:03 -0700

    revised meeting times

--- meeting_times.rb
+++ meeting_times.rb
@@ -1,38 +1,30 @@
-
 # [ [0, 1], [3, 5], [4, 8], [10, 12], [9, 10], [10, 11] ]
 
-# check if end time of current mtg is >= start and < end for another meeting
-# if so, check if end time is later than end date of the other meeting
-#   if it is, then block other meeting's start time up to current mtg's end time
-
-# check if start time of current mtg is equal to end time of another meeting
-# if so, then block other meeting's start time up to current mtg's end time
-
-def merge_ranges(arry_of_mtgs)
-  array_of_mtgs.sort
-
-  merged_meetings = [arry_of_mtgs[0]]
-
-  array_of_mtgs.each do |mtg_ary|
-    # check for overlap btwn 
+def merge_ranges(array_of_mtgs)
+  if array_of_mtgs.length < 2
+    return array_of_mtgs
   end
+
+  sorted_mtgs = array_of_mtgs.sort
+  merged_meetings = [sorted_mtgs[0]]
+
+  sorted_mtgs.each do |current_mtg_start, current_mtg_end|
+    last_merged_mtg_start, last_merged_mtg_end = merged_meetings.last
+
+    if last_merged_mtg_end >= current_mtg_end
+      next
+    elsif last_merged_mtg_end >= current_mtg_start
+      merged_meetings.last.replace([merged_meetings.last.first, current_mtg_end])
+    else
+      merged_meetings << [current_mtg_start, current_mtg_end]
+    end
+  end
+
+  p merged_meetings
 end
 
+# array_of_mtgs = [ [1, 2], [2, 3] ]
+array_of_mtgs = [ [0, 1], [3, 5], [4, 8], [10, 12], [9, 10] ]
+# array_of_mtgs = [ [1, 10], [2, 6], [3, 5], [7, 9] ]
 
-# def merge_ranges(arry_of_mtgs)
-#   condensed_range = []
-#   arry_of_mtgs.each_with_index do |mtg, index|
-#     current_start_time, current_end_time = mtg.first, mtg.last
-
-#     array_of_mtgs[index+1...-1].each do |other_mtg|
-#       start_time = other_mtg.first
-#       end_time = other_mtg.last
-      
-#       if current_end_time > start_time && current_end_time <= end_time
-#         mtg_block = [current_start_time, end_time]
-#       end
-#     end
-
-#     condensed_range << mtg_block
-#   end
-# end
+merge_ranges(array_of_mtgs)
